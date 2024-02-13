0c1637c419586057bc76024222f5a34a55dbf309
lucas-caminha <lucas_santoscaminha@hotmail.com>
2024-02-13 17:34:23 -0300

    commit inicial do projeto

---
+++ src/main/java/br/com/pagamento/PagamentosServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentosServiceApplication.class, args);
+	}
+
+}
